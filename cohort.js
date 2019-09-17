611cd0fbeef8a93bedc7566462182014891ec9c9
Mark Carlson <mark.carlson@takeoff.com>
2019-09-16 23:39:11 -0700

    Adds content from week 1 - day 1

---
+++ cohort.js
@@ -0,0 +1,141 @@
+const students = [{
+    name: "Anjali",
+    location: "Walnut Creek",
+    keywords: ["Works in San Bruno", "Has taken other classes", "Designer at Walmart", "Wants to learn to code too"],
+    isInTech: true
+}, {
+    name: "Cassie",
+    location: "Hayward",
+    keywords: ["Change industries", "Education"],
+    isInTech: false
+}, {
+    name: "Sara",
+    location: "Berkeley, Novato",
+    keywords: ["Career change", "Runs a maker space"],
+    isInTech: false
+}, {
+    name: "Mone",
+    location: "Oakland",
+    keywords: ["Graduated in media studies in UK", "Liked web dev classes"],
+    isInTech: false
+}, {
+    name: "Jill",
+    location: "Berkeley",
+    keywords: ["Works at dog day care", "Career change"],
+    isInTech: false
+}, {
+    name: "Christian",
+    location: "Richmond",
+    keywords: ["Race team/builds engines", "Wants to build biz with eCom"],
+    isInTech: false
+}, {
+    name: "Andrew",
+    location: "Fairfield",
+    keywords: ["Helping at family restaurant", "Gain skills for remote work"],
+    isInTech: false
+}, {
+    name: "Kevin",
+    location: "Walnut Creek",
+    keywords: ["Has studied javascript", "Interior design", "Looking for certification"],
+    isInTech: true
+}, {
+    name: "Kyle",
+    location: "Half Moon Bay",
+    keywords: ["Majored in CS game design", "Construction", "Wants to avoid injuries"],
+    isInTech: false
+}, {
+    name: "Rodson",
+    location: "Oakland/Emeryville",
+    keywords: ["Adobe - application support", "Wants to learn FS dev skills to transition at Adobe"],
+    isInTech: true
+}, {
+    name: "John",
+    location: "San Ramon",
+    keywords: ["Site director for after school enrichment program","Change careers"],
+    isInTech: false
+}, {
+    name: "Riley",
+    location: "Berkeley",
+    keywords: ["Works with Rodson", "Biz undergrad degree", "Wants to expand into dev"],
+    isInTech: true
+}, {
+    name: "Megan",
+    location: "Oakland/SF",
+    keywords: ["Case manager for previously homeless families", "change career"],
+    isInTech: false
+}, {
+    name: "Raissa",
+    location: "Alameda",
+    keywords: ["Manager at Walgreens", "Working towards a BS in Math", "Wants to add web dev certification"],
+    isInTech: false
+}, {
+    name: "Neil",
+    location: "Alameda",
+    keywords: ["Works at Hotel", "Inspired by hotel guest to take up web dev"],
+    isInTech: false
+}, {
+    name: "Danielle",
+    location: "Berkeley",
+    keywords: ["From Israel", "Following a dream", "Has ideas for building apps", "Financial freedom", "UCB community organizer"],
+    isInTech: false
+}, {
+    name: "Chris",
+    location: "Oakland/Lafayette",
+    keywords: ["From Chicago", "Staying in an AirBNB", "Health Benefits Consulting", "New career"],
+    isInTech: false
+}, {
+    name: "Jimmy",
+    location: "SF",
+    keywords: ["From Colombia", "Wants to have skills to make things", "Lives in hostel"],
+    isInTech: false
+}, {
+    name: "Lila",
+    location: "Walnut Creek",
+    keywords: ["From Algeria", "Store manager @ Sunglass Hut", "Assoc deg in HR/Management", "Taking the class with other coworkers"],
+    isInTech: false
+}, {
+    name: "Kaleb",
+    location: "Tracy",
+    keywords: ["Real Estate", "CSUEB/Hayward", "Wants the challenge"],
+    isInTech: false
+}, {
+    name: "Sy",
+    location: "Alameda",
+    keywords: ["Name pronounced 'See'", "UCSF - Parking lot security", "Amazon seller", "Has friends in tech", "Has an 'IN' in tech!"],
+    isInTech: false
+}, {
+    name: "Cody",
+    location: "Berkeley",
+    keywords: ["Went to UCB", "Works at Adventure Travel company", "Logistics", "Jealous of his friends", "Wants to improve skills for career"],
+    isInTech: false
+}, {
+    name: "Alex",
+    location: "Marin/SF",
+    keywords: ["Works at parent's movie theater", "Expand skill set"],
+    isInTech: false
+}, {
+    name: "Ashton",
+    location: "Oakland",
+    keywords: ["Works in south Fremont", "Remarketing/Sales @ Tesla", "Bored", "Creative outlet", "flexible schedule", "Certificate"],
+    isInTech: false
+}, {
+    name: "Tinashe",
+    location: "Albany",
+    keywords: ["From Zimbabwe", "UCB Extension", "Data Engineer", "More skills", "Self taught"],
+    isInTech: true
+}, {
+    name: "Ashish",
+    location: "San Pablo",
+    keywords: ["From Nepal", "Student", "Laney College student", "Working on CS degree"],
+    isInTech: false
+}, {
+    name: "Jeff",
+    location: "Walnut Creek",
+    keywords: ["Works in Berkeley", "Works on manufacturing high end speakers", "Digital sys support tech", "Wants to get into software engineering for his industry"],
+    isInTech: true
+}, {
+    name: "Ben",
+    location: "Hercules",
+    keywords: ["Works at St Mary's Hospital in the OR", "Bro is Sr. Dev"],
+    isInTech: false
+}]
